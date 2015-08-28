71049315c800314daaa1375f3888159ca06c9d67
maksdpua <maksdpua@gmail.com>
2015-08-28 17:34:57 +0300

    Add ViewControllerWithAddFunc

---
+++ Table.Maks.Shvec/ViewControllers/TMSViewControllerWithAddFunc/TMSViewControllerWithAddFunc.h
@@ -0,0 +1,13 @@
+//
+//  TMSViewControllerWithAddFunc.h
+//  Table.Maks.Shvec
+//
+//  Created by Maks on 8/28/15.
+//  Copyright © 2015 Maks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMSViewControllerWithAddFunc : UIViewController
+
+@end
